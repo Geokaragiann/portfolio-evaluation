9838a251a8da44e2d4dae0b8948bfe4820e09df4
Giorgos Karagiannidis <geokaragiann@outlook.com>
2025-10-18 17:24:46 +0300

    added standard deviation and sharpe ratio

--- src/utils.py
+++ src/utils.py
@@ -20,9 +20,13 @@ def geometric_portfolio_returns(daily_returns: pd.DataFrame, weights: np.ndarray
     return average_annual_return, annual_returns
 
 
-def standard_seviation(daily_prices: pd.DataFrame, weights: np.ndarray) -> float:
+def standard_deviation(daily_returns: pd.DataFrame, weights: np.ndarray) -> float:
     """
-     Given daily stock data and weights for each security, this function calculates and 
+    Given daily stock data and weights for each security, this function calculates and 
     returns the standard deviation of the portfolio.
     """
 
+    covariance_matrix: pd.DataFrame = daily_returns.cov()
+    daily_std = np.sqrt(weights.T @ covariance_matrix @ weights)
+    annualized_std = daily_std * np.sqrt(252) # total rows after cleaning = 5029. 5029/20 years = 251.45. However, I use 252 to avoid underestimating
+    return annualized_std
